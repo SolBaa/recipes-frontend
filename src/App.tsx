0ed653dfa7254e89441fe69cf73eaa0a4c4a57dd
Sol Battaglia <solbattaglia@gmail.com>
2023-08-31 10:00:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react'
+import './App.css'
+
+const App = (): JSX.Element => {
+  return (
+    <div className='mainTitle'>
+      <h1 >Recetas de Sol </h1>
+    </div>
+  )
+}
+export default App
