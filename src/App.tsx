b9c71e7f723ff358e5dbcc37c461343a0071804e
Sol Battaglia <solbattaglia@gmail.com>
2023-09-01 13:05:30 -0300

    Update template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const App = (): JSX.Element => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+      .then(async res => await res.json())
+      .then((res) => {
+        console.log(res)
+        setData(res.results)
+      })
+      .catch((error) => {
+        console.error('Error:', error)
+      }
+      )
+  }, [])
+
   return (
     <div className='mainTitle'>
       <h1 >Recetas de Sol </h1>
+
+    {JSON.stringify(data)}
     </div>
   )
 }
